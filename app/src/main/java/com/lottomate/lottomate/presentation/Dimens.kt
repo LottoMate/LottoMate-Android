e71aedac25ee5a3e4e0cbb4e40ba53aff0400171
w36495 <w36495@naver.com>
2024-07-30 16:19:06 +0900

    style : Radius 에 대한 dimens 설정

---
+++ app/src/main/java/com/lottomate/lottomate/presentation/Dimens.kt
@@ -0,0 +1,23 @@
+package com.lottomate.lottomate.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.Dp
+import com.lottomate.lottomate.R
+
+object Dimens {
+    val RadiusExtraSmall: Dp
+        @Composable get() = dimensionResource(id = R.dimen.radius_extra_small)
+
+    val RadiusSmall: Dp
+        @Composable get() = dimensionResource(id = R.dimen.radius_small)
+
+    val RadiusMedium: Dp
+        @Composable get() = dimensionResource(id = R.dimen.radius_medium)
+
+    val RadiusLarge: Dp
+        @Composable get() = dimensionResource(id = R.dimen.radius_large)
+
+    val RadiusExtraLarge: Dp
+        @Composable get() = dimensionResource(id = R.dimen.radius_extra_large)
+}
