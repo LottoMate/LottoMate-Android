49c44f87c97aba6fecfa6c7238db04219927d638
w36495 <w36495@naver.com>
2024-08-13 17:45:30 +0900

    refactor : 로또 금액 천의 자리 숫자에 콤마 적용

--- app/src/main/java/com/lottomate/lottomate/data/mapper/LottoInfoMapper.kt
+++ app/src/main/java/com/lottomate/lottomate/data/mapper/LottoInfoMapper.kt
@@ -1,5 +1,6 @@
 package com.lottomate.lottomate.data.mapper
 
+import android.icu.text.DecimalFormat
 import com.lottomate.lottomate.data.model.Lotto645InfoEntity
 import com.lottomate.lottomate.data.model.Lotto720InfoEntity
 import com.lottomate.lottomate.presentation.screen.lottoinfo.model.Lotto645Info
@@ -10,9 +11,15 @@ object LottoInfoMapper {
         return Lotto645Info(
             lottoRndNum = lottoInfoEntity.lottoRndNum,
             drwtDate = lottoInfoEntity.drwtDate,
-            prizeMoney = lottoInfoEntity.prizeMoney,
-            drwtWinNum = lottoInfoEntity.drwtWinNum,
-            drwtMoney = lottoInfoEntity.drwtMoney,
+            prizeMoney = lottoInfoEntity.prizeMoney.map {
+                formatNumberWithCommas(it)
+            },
+            drwtWinNum = lottoInfoEntity.drwtWinNum.map {
+                formatNumberWithCommas(it)
+            },
+            drwtMoney = lottoInfoEntity.drwtMoney.map {
+                formatNumberWithCommas(it)
+            },
             drwtNum = lottoInfoEntity.drwtNum,
             drwtBonusNum = lottoInfoEntity.drwtBonusNum,
             drwtSaleMoney = lottoInfoEntity.drwtSaleMoney
@@ -23,9 +30,21 @@ object LottoInfoMapper {
         return Lotto720Info(
             lottoRndNum = lottoInfoEntity.lottoRndNum,
             drwtDate = lottoInfoEntity.drwtDate,
-            drwtWinNum = lottoInfoEntity.drwtWinNum,
+            drwtWinNum = lottoInfoEntity.drwtWinNum.map {
+                formatNumberWithCommas(it)
+            },
             drwtNum = lottoInfoEntity.drwtNum,
             drwtBonusNum = lottoInfoEntity.drwtBonusNum
         )
+    }
+
+    private fun formatNumberWithCommas(num: Long): String {
+        val formatter = DecimalFormat("#,###")
+        return formatter.format(num)
+    }
+
+    private fun formatNumberWithCommas(num: Int): String {
+        val formatter = DecimalFormat("#,###")
+        return formatter.format(num)
     }
 }
