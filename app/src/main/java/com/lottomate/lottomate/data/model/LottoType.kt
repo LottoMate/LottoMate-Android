0ece82b30c7da31fb50af8d77e03526c65101727
w36495 <w36495@naver.com>
2024-09-04 19:58:12 +0900

    [ STYLE ] 로또 타입 선언 순서 변경

--- app/src/main/java/com/lottomate/lottomate/data/model/LottoType.kt
+++ app/src/main/java/com/lottomate/lottomate/data/model/LottoType.kt
@@ -1,7 +1,7 @@
 package com.lottomate.lottomate.data.model
 
 enum class LottoType(val num: Int) {
-    L645(645), L720(720), S500(500), S1000(1_000), S2000(2_000);
+    L645(645), L720(720), S2000(2_000), S1000(1_000), S500(500);
 
     companion object {
         fun findLottoType(index: Int): LottoType = entries[index]
