c2a496b3ef4701fc08b0351bb4be3f21e00f257a
w36495 <w36495@naver.com>
2024-09-14 08:05:11 +0900

    [ REFACTOR ] 스피또 Mock Data 수정

--- app/src/main/java/com/lottomate/lottomate/presentation/screen/lottoinfo/model/LottoInfo.kt
+++ app/src/main/java/com/lottomate/lottomate/presentation/screen/lottoinfo/model/LottoInfo.kt
@@ -48,75 +48,79 @@ data class SpeettoInfoDetail(
     override val lottoDate: String,
 ): LottoInfo
 
-val SpeettoMockDatas = listOf(
-    SpeettoInfoDetail(
-        rank = 1,
-        lottoRound = 55,
-        store = "진짜 이름이 아주 긴 판매점 보라매점보라매점",
-        lottoDate = "2024.08.21",
-        hasInterview = false
-    ),
-    SpeettoInfoDetail(
-        rank = 1,
-        lottoRound = 55,
-        store = "진짜 이름이 아주 긴 판매점 보라매점",
-        lottoDate = "2024.08.21",
-        hasInterview = true
-    ),
-    SpeettoInfoDetail(
-        rank = 1,
-        lottoRound = 55,
-        store = "CU알리앙스점",
-        lottoDate = "2024.08.14",
-        hasInterview = false
-    ),
-    SpeettoInfoDetail(
-        rank = 1,
-        lottoRound = 55,
-        store = "CU알리앙스점",
-        lottoDate = "2024.08.14",
-        hasInterview = true
-    ),
-    SpeettoInfoDetail(
-        rank = 1,
-        lottoRound = 55,
-        store = "신율편의마트",
-        lottoDate = "2024.07.29",
-        hasInterview = true
-    ),
-    SpeettoInfoDetail(
-        rank = 1,
-        lottoRound = 55,
-        store = "신율편의마트",
-        lottoDate = "2024.07.29",
-        hasInterview = false
-    ),
-    SpeettoInfoDetail(
-        rank = 2,
-        lottoRound = 55,
-        store = "GS25명장화목점",
-        lottoDate = "2024.08.28",
-        hasInterview = false
-    ),
-    SpeettoInfoDetail(
-        rank = 2,
-        lottoRound = 55,
-        store = "공단로또명당",
-        lottoDate = "2024.08.23",
-        hasInterview = false
-    ),
-    SpeettoInfoDetail(
-        rank = 2,
-        lottoRound = 55,
-        store = "행운복권마트",
-        lottoDate = "2024.08.23",
-        hasInterview = false
-    ),
-    SpeettoInfoDetail(
-        rank = 2,
-        lottoRound = 55,
-        store = "남원도통복권방",
-        lottoDate = "2024.08.14",
-        hasInterview = false
-    ),
+val SpeettoMockDatas = SpeettoInfo(
+    currentPage = 1,
+    lastPage = 69,
+    details = listOf(
+        SpeettoInfoDetail(
+            rank = 1,
+            lottoRound = 55,
+            store = "진짜 이름이 아주 긴 판매점 보라매점보라매점",
+            lottoDate = "2024.08.21",
+            hasInterview = false
+        ),
+        SpeettoInfoDetail(
+            rank = 1,
+            lottoRound = 55,
+            store = "진짜 이름이 아주 긴 판매점 보라매점",
+            lottoDate = "2024.08.21",
+            hasInterview = true
+        ),
+        SpeettoInfoDetail(
+            rank = 1,
+            lottoRound = 55,
+            store = "CU알리앙스점",
+            lottoDate = "2024.08.14",
+            hasInterview = false
+        ),
+        SpeettoInfoDetail(
+            rank = 1,
+            lottoRound = 55,
+            store = "CU알리앙스점",
+            lottoDate = "2024.08.14",
+            hasInterview = true
+        ),
+        SpeettoInfoDetail(
+            rank = 1,
+            lottoRound = 55,
+            store = "신율편의마트",
+            lottoDate = "2024.07.29",
+            hasInterview = true
+        ),
+        SpeettoInfoDetail(
+            rank = 1,
+            lottoRound = 55,
+            store = "신율편의마트",
+            lottoDate = "2024.07.29",
+            hasInterview = false
+        ),
+        SpeettoInfoDetail(
+            rank = 2,
+            lottoRound = 55,
+            store = "GS25명장화목점",
+            lottoDate = "2024.08.28",
+            hasInterview = false
+        ),
+        SpeettoInfoDetail(
+            rank = 2,
+            lottoRound = 55,
+            store = "공단로또명당",
+            lottoDate = "2024.08.23",
+            hasInterview = false
+        ),
+        SpeettoInfoDetail(
+            rank = 2,
+            lottoRound = 55,
+            store = "행운복권마트",
+            lottoDate = "2024.08.23",
+            hasInterview = false
+        ),
+        SpeettoInfoDetail(
+            rank = 2,
+            lottoRound = 55,
+            store = "남원도통복권방",
+            lottoDate = "2024.08.14",
+            hasInterview = false
+        ),
+    )
 )
