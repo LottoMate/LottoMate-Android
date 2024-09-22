d4ab645118775b27cf8cb8fc423fc2eca5b1acbb
w36495 <w36495@naver.com>
2024-09-22 16:10:52 +0900

    [ REFACTOR ] LottoInfo Entity에 각 1인 당첨금 변수 추가

--- app/src/main/java/com/lottomate/lottomate/data/model/LottoInfoEntity.kt
+++ app/src/main/java/com/lottomate/lottomate/data/model/LottoInfoEntity.kt
@@ -18,14 +18,19 @@ data class Lotto645InfoEntity(
     val totalSalesPrice: Long,
     val p1Jackpot: Long,
     val p1WinnrCnt: Int,
+    val p1PerJackpot:Long,
     val p2Jackpot: Long,
     val p2WinnrCnt: Int,
+    val p2JPerJackpot: Long,
     val p3Jackpot: Long,
     val p3WinnrCnt: Int,
+    val p3PerJackpot: Long,
     val p4Jackpot: Long,
     val p4WinnrCnt: Int,
+    val p4PerJackpot: Long,
     val p5Jackpot: Long,
     val p5WinnrCnt: Int,
+    val p5PerJackpot: Long,
 ): LottoInfoEntity
 
 @Serializable
@@ -44,5 +49,4 @@ data class Lotto720InfoEntity(
     val p5WinnrCnt: Int,
     val p6WinnrCnt: Int,
     val p7WinnrCnt: Int,
-    val p8WinnrCnt: Int,
 ): LottoInfoEntity
