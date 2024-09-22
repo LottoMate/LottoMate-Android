0a4828f08fad640eaf2a0899d546be8899e8753f
w36495 <w36495@naver.com>
2024-09-22 17:35:25 +0900

    [ STYLE ] 불필요한 코드 삭제

--- app/src/main/java/com/lottomate/lottomate/presentation/screen/lottoinfo/LottoInfoViewModel.kt
+++ app/src/main/java/com/lottomate/lottomate/presentation/screen/lottoinfo/LottoInfoViewModel.kt
@@ -15,7 +15,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
