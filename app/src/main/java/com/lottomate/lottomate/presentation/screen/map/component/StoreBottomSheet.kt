78f5e01ff2935bfbc997bbfc2d0ca938a879637f
w36495 <w36495@naver.com>
2024-09-29 14:40:12 +0900

    [ REFACTOR ] BottomSheet가 보여지는 높이 변경

--- app/src/main/java/com/lottomate/lottomate/presentation/screen/map/component/StoreBottomSheet.kt
+++ app/src/main/java/com/lottomate/lottomate/presentation/screen/map/component/StoreBottomSheet.kt
@@ -9,8 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -112,7 +112,7 @@ private fun StoreInfoBottomSheetContent(
 
     Column(
         modifier = modifier
-            .requiredHeight(bottomSheetHeight.dp)
+            .heightIn(max = bottomSheetHeight.dp)
             .verticalScroll(rememberScrollState()),
     ) {
         Box(
