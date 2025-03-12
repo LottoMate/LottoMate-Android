a20086261e9e76141bcc723db9a8aeda06bd47f3
w36495 <w36495@naver.com>
2025-03-12 19:48:26 +0900

    [ FEAT ] 온보딩 View 구현 및 권한 화면으로 이동 구현

---
+++ app/src/main/java/com/lottomate/lottomate/presentation/screen/onboarding/OnboardingActivity.kt
@@ -0,0 +1,40 @@
+package com.lottomate.lottomate.presentation.screen.onboarding
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.lottomate.lottomate.presentation.screen.main.MainActivity
+import com.lottomate.lottomate.presentation.ui.LottoMateTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OnboardingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            LottoMateTheme {
+                OnboardingRoute()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            val intent = Intent(this@OnboardingActivity, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+
+            startActivity(intent)
+            finish()
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+}
