a24fffee05ce8db2b0fcfda972179dd1b8dc9d8a
w36495 <w36495@naver.com>
2025-03-15 17:34:13 +0900

    [ REFACTOR ] 로또 당첨 등수 별 UI 수정

--- app/src/main/java/com/lottomate/lottomate/presentation/screen/scanResult/LottoScanResultScreen.kt
+++ app/src/main/java/com/lottomate/lottomate/presentation/screen/scanResult/LottoScanResultScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,7 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -23,7 +22,6 @@ import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -36,11 +34,14 @@ import com.lottomate.lottomate.presentation.component.LottoMateButtonProperty
 import com.lottomate.lottomate.presentation.component.LottoMateSolidButton
 import com.lottomate.lottomate.presentation.component.LottoMateText
 import com.lottomate.lottomate.presentation.res.Dimens
+import com.lottomate.lottomate.presentation.ui.LottoMateGray100
 import com.lottomate.lottomate.presentation.ui.LottoMateGray110
+import com.lottomate.lottomate.presentation.ui.LottoMateGray120
 import com.lottomate.lottomate.presentation.ui.LottoMateRed50
 import com.lottomate.lottomate.presentation.ui.LottoMateTheme
 import com.lottomate.lottomate.presentation.ui.LottoMateWhite
 import com.lottomate.lottomate.utils.DateUtils
+import com.lottomate.lottomate.utils.StringUtils
 
 @Composable
 fun LottoScanResultRoute(
@@ -85,16 +86,23 @@ private fun LottoScanResultScreen(
                 )
             }
 
+            LottoScanResultUiState.CelebrationLoading -> {
+                LottoScanResultCelebrationLoading(
+                    modifier = Modifier.align(Alignment.Center),
+                )
+            }
+
             LottoScanResultUiState.NotYet -> {
                 // 당첨 발표 전
                 LottoScanResultNotYet(
                     moveToHome = moveToHome,
+                    onClick = onBackPressed,
                 )
             }
 
             is LottoScanResultUiState.Success -> {
                 val winResultInfo = uiState.data
-                val isFailed = winResultInfo.all { it == -1 }
+                val isFailed = winResultInfo.all { it == null }
 
                 Column(
                     modifier = modifier.padding(horizontal = Dimens.DefaultPadding20),
@@ -104,6 +112,7 @@ private fun LottoScanResultScreen(
                         true -> {
                             // 미당첨 화면
                             LottoScanResultWin(
+                                modifier = Modifier.weight(1f),
                                 isFailed = true,
                                 rank = -1,
                                 isLast = true,
@@ -112,13 +121,18 @@ private fun LottoScanResultScreen(
                         }
                         false -> {
                             // 당첨 화면
-                            val realWinResultInfo = winResultInfo.sorted().filter { it != -1 }
+                            val realWinResultInfo = winResultInfo
+                                .filterNotNull()
+                                .sortedBy { it.rank }
+                                .filter { it.rank != -1 }
 
-                            var currentIndex by remember { mutableStateOf(0) }
+                            var currentIndex by remember { mutableIntStateOf(0) }
 
                             LottoScanResultWin(
+                                modifier = Modifier.weight(1f),
                                 isFailed = false,
-                                rank = realWinResultInfo[currentIndex],
+                                rank = realWinResultInfo[currentIndex].rank,
+                                price = realWinResultInfo[currentIndex].price,
                                 isLast = currentIndex == realWinResultInfo.lastIndex,
                                 onNext = {
                                     if (currentIndex < realWinResultInfo.lastIndex) {
@@ -135,11 +149,11 @@ private fun LottoScanResultScreen(
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 private fun LottoScanResultNotYet(
     modifier: Modifier = Modifier,
-    moveToHome: () -> Unit = {},
+    onClick: () -> Unit,
+    moveToHome: () -> Unit,
 ) {
     Column(
         modifier = modifier.padding(horizontal = Dimens.DefaultPadding20),
@@ -182,7 +196,7 @@ private fun LottoScanResultNotYet(
             )
 
             AsyncImage(
-                model = R.drawable.img_lotto_scan_result_slo_cony,
+                model = R.drawable.img_lotto_result_loading,
                 contentDescription = null,
                 modifier = Modifier
                     .padding(top = 40.dp)
@@ -214,7 +228,7 @@ private fun LottoScanResultNotYet(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 24.dp),
-                onClick = moveToHome,
+                onClick = onClick,
             )
         }
     }
@@ -225,15 +239,18 @@ private fun LottoScanResultWin(
     modifier: Modifier = Modifier,
     isFailed: Boolean,
     rank: Int,
+    price: String? = null,
     isLast: Boolean,
     onNext: (() -> Unit)? = null,
     onComplete: () -> Unit,
 ) {
     Column(
         modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         LottoMateText(
-            text = if (isFailed) "이런.. 아쉽게 미당첨" else {
+            text = if (isFailed) "아쉽게 미당첨" else {
                 when (rank) {
                     1 -> "로또 1등 당첨"
                     2 -> "로또 2등 당첨"
@@ -243,62 +260,95 @@ private fun LottoScanResultWin(
                     else -> ""
                 }
             },
-            style = LottoMateTheme.typography.title2,
+            style = when (isFailed) {
+                true -> LottoMateTheme.typography.title2
+                false -> LottoMateTheme.typography.title3
+                    .copy(color = LottoMateGray120)
+            },
             textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 92.dp.plus(Dimens.StatusBarHeight)),
+            modifier = Modifier.fillMaxWidth(),
         )
 
         LottoMateText(
-            text = if (isFailed) "다음 기회엔 꼭 당첨을 기원합니다." else {
-                when (rank) {
-                    1 -> "로또 1등 당첨"
-                    2 -> "로또 2등 당첨"
-                    3 -> "로또 3등 당첨"
-                    4 -> "로또 4등 당첨"
-                    5 -> "로또 5등 당첨"
-                    else -> ""
-                }
+            text = when (isFailed) {
+                true -> "다음 기회엔 꼭 당첨되기를 바라요!"
+                false -> price.plus("원") ?: ""
             },
-            style = LottoMateTheme.typography.headline1
-                .copy(color = LottoMateGray110),
+            style = when (isFailed) {
+                true -> LottoMateTheme.typography.headline1
+                    .copy(color = LottoMateGray110)
+                false -> LottoMateTheme.typography.display2
+            },
             textAlign = TextAlign.Center,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 2.dp),
         )
 
-        Spacer(modifier = Modifier.weight(1f))
-
-        AsyncImage(
-            model = if (isFailed) R.drawable.img_lotto_scan_result_slo_cony_failed else R.drawable.img_lotto_scan_result_slo_cony,
-            contentDescription = null,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Column {
-            BannerCard(
-                onClickBanner = {
-
+        if (!isFailed && rank != 5) {
+            LottoMateText(
+                text = when(rank) {
+                    in 1..4 -> StringUtils.formatLottoPrize(price?.replace(",", "")?.toLong() ?: 0L)
+                    else -> ""
                 },
-            )
-
-            LottoMateSolidButton(
-                text = if (isLast) "확인" else "다음",
-                buttonSize = LottoMateButtonProperty.Size.LARGE,
-                buttonShape = LottoMateButtonProperty.Shape.NORMAL,
+                style = LottoMateTheme.typography.headline2
+                    .copy(color = LottoMateGray100),
+                textAlign = TextAlign.Center,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 24.dp),
-                onClick = {
-                    if (isLast) onComplete()
-                    else onNext?.invoke()
-                },
+                    .padding(top = 2.dp),
             )
         }
+
+        AsyncImage(
+            model = if (isFailed) {
+                R.drawable.img_lotto_result00
+            } else {
+                when (rank) {
+                    1 -> R.drawable.img_lotto_result03
+                    2, 3 -> R.drawable.img_lotto_result02
+                    4, 5 -> R.drawable.img_lotto_result01
+                    else -> R.drawable.img_lotto_result_loading
+                }
+            },
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp),
+        )
+
+        LottoMateText(
+            text = "당첨금 수령 방법은 가이드를 확인해 주세요",
+            style = LottoMateTheme.typography.body1
+                .copy(color = LottoMateGray110),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp),
+        )
+    }
+
+    Column(
+        modifier = Modifier.padding(bottom = 36.dp),
+    ) {
+        BannerCard(
+            onClickBanner = {
+
+            },
+        )
+
+        LottoMateSolidButton(
+            text = if (isLast) "확인" else "다음",
+            buttonSize = LottoMateButtonProperty.Size.LARGE,
+            buttonShape = LottoMateButtonProperty.Shape.NORMAL,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 36.dp),
+            onClick = {
+                if (isLast) onComplete()
+                else onNext?.invoke()
+            },
+        )
     }
 }
 
@@ -318,7 +368,30 @@ private fun LottoScanResultLoading(
         )
 
         AsyncImage(
-            model = R.drawable.img_lotto_scan_result_slo_cony,
+            model = R.drawable.img_lotto_result_loading,
+            contentDescription = null,
+            modifier = Modifier.padding(top = 32.dp),
+        )
+    }
+}
+
+@Composable
+private fun LottoScanResultCelebrationLoading(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        LottoMateText(
+            text = "어라?\n이 느낌은...",
+            style = LottoMateTheme.typography.title2,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        AsyncImage(
+            model = R.drawable.img_lotto_result_loading,
             contentDescription = null,
             modifier = Modifier.padding(top = 32.dp),
         )
