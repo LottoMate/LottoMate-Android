7ca9e9cab91e23938c65374dbaba94e1f888bbf1
w36495 <w36495@naver.com>
2025-03-21 16:22:25 +0900

    [ STYLE ] AnnotatedString Text 추가

--- app/src/main/java/com/lottomate/lottomate/presentation/component/Texts.kt
+++ app/src/main/java/com/lottomate/lottomate/presentation/component/Texts.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -62,6 +63,57 @@ fun LottoMateText(
 //        modifier = modifier.then(baselineModifier),
         modifier = modifier,
         color = color,
+        fontSize = size,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        fontFamily = fontFamily,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        onTextLayout = onTextLayout,
+        style = style.copy(fontSize = size),
+    )
+}
+
+@Composable
+fun LottoMateAnnotatedText(
+    annotatedString: AnnotatedString,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current,
+) {
+//    val textStyle: LottoMateTextStyle = lottoMateTextStyle(style = style)
+//    val baselineModifier = if (textStyle != LottoMateTextStyle.UNSPECIFIED) {
+//        val top = textStyle.firstBaseLineToTop.value.dp
+//        val bottom = textStyle.lastBaseLineToBottom.value.dp
+//        Modifier.paddingFromBaseline(top = top, bottom = bottom)
+//    } else {
+//        Modifier
+//    }
+
+    val size = with(LocalDensity.current) { style.fontSize.value.dp.toSp() }
+
+
+    Text(
+        text = annotatedString,
+        modifier = modifier,
+        color = annotatedString?.let { Color.Unspecified } ?: color,
         fontSize = size,
         fontStyle = fontStyle,
         fontWeight = fontWeight,
