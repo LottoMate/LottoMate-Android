81a92e748b2318a38e16d6b974c22ff049a46a1a
w36495 <w36495@naver.com>
2025-03-23 22:02:53 +0900

    [ STYLE ] BannerCard 수정

--- app/src/main/java/com/lottomate/lottomate/presentation/component/Cards.kt
+++ app/src/main/java/com/lottomate/lottomate/presentation/component/Cards.kt
@@ -1,5 +1,6 @@
 package com.lottomate.lottomate.presentation.component
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -8,11 +9,11 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
@@ -20,23 +21,51 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lottomate.lottomate.R
+import com.lottomate.lottomate.presentation.navigation.LottoMateRoute
 import com.lottomate.lottomate.presentation.res.Dimens
+import com.lottomate.lottomate.presentation.screen.main.MainBottomTab
 import com.lottomate.lottomate.presentation.ui.LottoMateBlack
-import com.lottomate.lottomate.presentation.ui.LottoMateGray120
+import com.lottomate.lottomate.presentation.ui.LottoMateBlue5
+import com.lottomate.lottomate.presentation.ui.LottoMateGray90
 import com.lottomate.lottomate.presentation.ui.LottoMateTheme
 import com.lottomate.lottomate.presentation.ui.LottoMateWhite
 import com.lottomate.lottomate.presentation.ui.LottoMateYellow5
 import com.lottomate.lottomate.utils.dropShadow
 import com.lottomate.lottomate.utils.noInteractionClickable
+
+enum class BannerType(
+    val title: String = "",
+    val subTitle: String = "",
+    val backgroundColor: Color,
+    @DrawableRes val img: Int,
+    val route: LottoMateRoute,
+) {
+    // 지도
+    MAP(
+        title = "행운의 1등 로또\n어디서 샀을까?",
+        subTitle = "당첨 판매점 보기",
+        backgroundColor = LottoMateYellow5,
+        img = R.drawable.img_banner_map,
+        route = MainBottomTab.MAP.route,
+    ),
+    // 당첨자 가이드
+    WINNER_GUIDE(
+        title = "내가 당첨이 됐다면\n어떻게 해야할까",
+        subTitle = "당첨자 가이드 확인하기",
+        backgroundColor = LottoMateBlue5,
+        img = R.drawable.img_home_slo_cony,
+        route = LottoMateRoute.LottoWinnerGuide,
+    )
+}
 
 @Composable
 fun LottoMateCard(
@@ -73,13 +102,15 @@ fun LottoMateCard(
 @Composable
 fun BannerCard(
     modifier: Modifier = Modifier,
+    type: BannerType = BannerType.WINNER_GUIDE,
     onClickBanner: () -> Unit,
 ) {
     Box(
         modifier = modifier
+            .fillMaxWidth()
             .height(100.dp)
             .background(
-                color = LottoMateYellow5,
+                color = type.backgroundColor,
                 shape = RoundedCornerShape(Dimens.RadiusLarge)
             )
             .clickable(
@@ -88,33 +119,34 @@ fun BannerCard(
                 onClick = onClickBanner
             )
     ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 15.dp, start = 20.dp, end = 18.dp),
+                .align(Alignment.CenterStart)
+                .padding(start = 20.dp),
+            verticalArrangement = Arrangement.Center,
         ) {
-            Column {
-                LottoMateText(
-                    text = stringResource(id = R.string.banner_lotto_info_title),
-                    style = LottoMateTheme.typography.headline2,
-                )
+            LottoMateText(
+                text = type.title,
+                style = LottoMateTheme.typography.headline2,
+            )
 
-                Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(4.dp))
 
-                LottoMateText(
-                    text = stringResource(id = R.string.banner_lotto_info_sub_title),
-                    style = LottoMateTheme.typography.caption
-                        .copy(LottoMateGray120),
-                )
-            }
-
-            Image(
-                bitmap = ImageBitmap.imageResource(id = R.drawable.bn_pochi_lotto_info),
-                contentDescription = "Lotto Info Bottom Banner Image",
-                modifier = Modifier.padding(bottom = 7.dp),
+            LottoMateText(
+                text = type.subTitle,
+                style = LottoMateTheme.typography.caption
+                    .copy(LottoMateGray90),
             )
         }
+
+        Image(
+            bitmap = ImageBitmap.imageResource(id = type.img),
+            contentDescription = "Lotto Info Bottom Banner Image",
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 8.dp, end = 24.dp)
+                .size(width = 122.dp, height = 76.dp),
+        )
     }
 }
 
@@ -122,7 +154,9 @@ fun BannerCard(
 @Composable
 private fun BannerCardPreview() {
     LottoMateTheme {
-        BannerCard {
+        BannerCard(
+            type = BannerType.WINNER_GUIDE,
+        ) {
 
         }
     }
