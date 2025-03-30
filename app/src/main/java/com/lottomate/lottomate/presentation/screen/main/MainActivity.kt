f493e6365ad8592e966e175e29f8e9f04fca394c
w36495 <w36495@naver.com>
2025-03-30 22:03:31 +0900

    [ REFACTOR ] 인트로 보이기 전 흰 화면 제거

--- app/src/main/java/com/lottomate/lottomate/presentation/screen/main/MainActivity.kt
+++ app/src/main/java/com/lottomate/lottomate/presentation/screen/main/MainActivity.kt
@@ -5,26 +5,13 @@ import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.lifecycleScope
 import com.lottomate.lottomate.presentation.ui.LottoMateTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val INTRO_DISPLAY_DELAY = 500L
-    private var isShowingSplash = true
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        installSplashScreen().setKeepOnScreenCondition { isShowingSplash }
-        lifecycleScope.launch{
-            delay(INTRO_DISPLAY_DELAY)
-            isShowingSplash = false
-        }
 
         enableEdgeToEdge()
         setContent {
