35aabaad74193d10ae4a19600eaae1843ecffe14
w36495 <w36495@naver.com>
2025-04-14 15:21:45 +0900

    [ REFACTOR ] 공통으로 사용할 수 있도록 WebView Component 수정

--- app/src/main/java/com/lottomate/lottomate/presentation/component/Webviews.kt
+++ app/src/main/java/com/lottomate/lottomate/presentation/component/Webviews.kt
@@ -1,7 +1,10 @@
 package com.lottomate.lottomate.presentation.component
 
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,17 +12,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
-import com.lottomate.lottomate.R
 import com.lottomate.lottomate.presentation.res.Dimens
-import com.lottomate.lottomate.presentation.ui.LottoMateTheme
 import com.lottomate.lottomate.presentation.ui.LottoMateWhite
-import java.net.URLEncoder
 
 @Composable
-fun LottoMateNaverMapWebView(
-    place: String,
+fun LottoMateBasicWebView(
+    @StringRes title: Int,
+    url: String,
+    webViewClient: WebViewClient = WebViewClient(),
+    webChromeClient: WebChromeClient = WebChromeClient(),
     onBackPressed: () -> Unit,
 ) {
     val context = LocalContext.current
@@ -29,8 +31,6 @@ fun LottoMateNaverMapWebView(
             .fillMaxSize()
             .background(LottoMateWhite),
     ) {
-        val encodedPlace = URLEncoder.encode(place, "UTF-8")
-
         AndroidView(
             modifier = Modifier
                 .padding(top = Dimens.BaseTopPadding)
@@ -45,30 +45,22 @@ fun LottoMateNaverMapWebView(
                     settings.builtInZoomControls = true
                     settings.displayZoomControls = false
 
+                    clearCache(true) // 캐시 제거
+                    clearHistory()   // 기록 제거
+                    settings.cacheMode = WebSettings.LOAD_NO_CACHE // 캐시 사용하지 않도록 설정
 
-                    webViewClient = WebViewClient()
+                    this.webViewClient = webViewClient
+                    this.webChromeClient = webChromeClient
 
-                    if (place.contains("동행복권")) {
-                        loadUrl("https://m.map.naver.com/search2/search.naver?query=%EB%8F%99%ED%96%89%EB%B3%B5%EA%B6%8C&sm=hty&style=v5#/map/1/1706795753")
-                    } else loadUrl("https://m.map.naver.com/search2/search.naver?query=$encodedPlace&sm=hty&style=v5#/map/1")
+                    loadUrl(url)
                 }
-            }
+            },
         )
+
         LottoMateTopAppBar(
-            titleRes = R.string.guide_title,
+            titleRes = title,
             hasNavigation = true,
             onBackPressed = onBackPressed,
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun LottoMateWebViewPreview() {
-    LottoMateTheme {
-        LottoMateNaverMapWebView(
-            place = "www.naver.com",
-            onBackPressed = {},
         )
     }
 }
