8ea23a70f53de7ed93c73d0e8f586b55b1b0f9a2
w36495 <w36495@naver.com>
2025-05-16 12:42:37 +0900

    [ FEAT ] 배너 6종 추가

--- app/src/main/java/com/lottomate/lottomate/presentation/component/Cards.kt
+++ app/src/main/java/com/lottomate/lottomate/presentation/component/Cards.kt
@@ -34,13 +34,20 @@ import com.lottomate.lottomate.presentation.navigation.LottoMateRoute
 import com.lottomate.lottomate.presentation.res.Dimens
 import com.lottomate.lottomate.presentation.screen.main.MainBottomTab
 import com.lottomate.lottomate.presentation.ui.LottoMateBlack
+import com.lottomate.lottomate.presentation.ui.LottoMateBlue10
 import com.lottomate.lottomate.presentation.ui.LottoMateBlue5
+import com.lottomate.lottomate.presentation.ui.LottoMateGray20
 import com.lottomate.lottomate.presentation.ui.LottoMateGray90
+import com.lottomate.lottomate.presentation.ui.LottoMateGreen5
+import com.lottomate.lottomate.presentation.ui.LottoMatePeach10
+import com.lottomate.lottomate.presentation.ui.LottoMatePeach5
 import com.lottomate.lottomate.presentation.ui.LottoMateTheme
 import com.lottomate.lottomate.presentation.ui.LottoMateWhite
+import com.lottomate.lottomate.presentation.ui.LottoMateYellow10
 import com.lottomate.lottomate.presentation.ui.LottoMateYellow5
 import com.lottomate.lottomate.utils.dropShadow
 import com.lottomate.lottomate.utils.noInteractionClickable
+import kotlin.random.Random
 
 enum class BannerType(
     val title: String = "",
@@ -57,6 +64,13 @@ enum class BannerType(
         img = R.drawable.img_banner_map,
         route = MainBottomTab.MAP.route,
     ),
+    MAP2(
+        title = "로또 1등 당첨\n기 받아가요",
+        subTitle = "당첨 판매점 보기",
+        backgroundColor = LottoMatePeach5,
+        img = R.drawable.img_banner02,
+        route = MainBottomTab.MAP.route,
+    ),
     // 당첨자 가이드
     WINNER_GUIDE(
         title = "내가 당첨이 됐다면\n어떻게 해야할까",
@@ -64,7 +78,48 @@ enum class BannerType(
         backgroundColor = LottoMateBlue5,
         img = R.drawable.img_home_slo_cony,
         route = LottoMateRoute.LottoWinnerGuide,
-    )
+    ),
+    DRAW_LOTTO(
+        title = "두구두구두구\n로또 당첨 발표",
+        subTitle = "당첨 로또 정보 확인하기",
+        backgroundColor = LottoMateGreen5,
+        img = R.drawable.img_banner_map,
+        route = MainBottomTab.MAP.route,
+    ),
+    CHECK_MY_LOTTO_RESULT(
+        title = "내 로또 당첨 결과\n빠르게 보고 싶다면?",
+        subTitle = "QR로 당첨 확인하기",
+        backgroundColor = LottoMateBlue10,
+        img = R.drawable.img_banner05,
+        route = MainBottomTab.MAP.route,
+    ),
+    RANDOM_LOTTO_NUMBER(
+        title = "나만의 로또 번호\n뽑아봐요!",
+        subTitle = "로또 번호 뽑으러 가기",
+        backgroundColor = LottoMateYellow10,
+        img = R.drawable.img_banner06,
+        route = MainBottomTab.MAP.route,
+    ),
+    REQUEST_WINNER_LOTTO_STORE(
+        title = "우리 동네 로또 명당\n알고 싶어요",
+        subTitle = "지도 오픈 요청하기",
+        backgroundColor = LottoMatePeach10,
+        img = R.drawable.img_banner07,
+        route = MainBottomTab.MAP.route,
+    ),
+    VOTE_IDEA(
+        title = "당첨이 되면\n뭐가 제일 하고 싶어?",
+        subTitle = "메이트 투표 아이디어 접수",
+        backgroundColor = LottoMateGray20,
+        img = R.drawable.img_banner08,
+        route = MainBottomTab.MAP.route,
+    );
+
+    companion object {
+        fun find(index: Int): BannerType {
+            return entries.find { it.ordinal == index } ?: MAP
+        }
+    }
 }
 
 @Composable
@@ -105,18 +160,21 @@ fun BannerCard(
     type: BannerType = BannerType.WINNER_GUIDE,
     onClickBanner: () -> Unit,
 ) {
+    val randomBanner = Random(7).nextInt()
+    val randomBannerType = BannerType.find(randomBanner)
+
     Box(
         modifier = modifier
             .fillMaxWidth()
             .height(100.dp)
             .background(
-                color = type.backgroundColor,
+                color = randomBannerType.backgroundColor,
                 shape = RoundedCornerShape(Dimens.RadiusLarge)
             )
             .clickable(
                 interactionSource = remember { MutableInteractionSource() },
                 indication = null,
-                onClick = onClickBanner
+                onClick = { onClickBanner() }
             )
     ) {
         Column(
@@ -126,21 +184,21 @@ fun BannerCard(
             verticalArrangement = Arrangement.Center,
         ) {
             LottoMateText(
-                text = type.title,
+                text = randomBannerType.title,
                 style = LottoMateTheme.typography.headline2,
             )
 
             Spacer(modifier = Modifier.height(4.dp))
 
             LottoMateText(
-                text = type.subTitle,
+                text = randomBannerType.subTitle,
                 style = LottoMateTheme.typography.caption
                     .copy(LottoMateGray90),
             )
         }
 
         Image(
-            bitmap = ImageBitmap.imageResource(id = type.img),
+            bitmap = ImageBitmap.imageResource(id = randomBannerType.img),
             contentDescription = "Lotto Info Bottom Banner Image",
             modifier = Modifier
                 .align(Alignment.BottomEnd)
@@ -155,9 +213,8 @@ fun BannerCard(
 private fun BannerCardPreview() {
     LottoMateTheme {
         BannerCard(
-            type = BannerType.WINNER_GUIDE,
-        ) {
-
-        }
+            type = BannerType.MAP,
+            onClickBanner = {}
+        )
     }
 }
