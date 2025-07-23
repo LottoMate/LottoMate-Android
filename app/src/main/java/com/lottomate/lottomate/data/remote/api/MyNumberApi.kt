3d0071fd7ec36bd316ad9066ee42051d7dc8de19
w36495 <w36495@naver.com>
2025-07-23 16:04:05 +0900

    [ REFACTOR ] 내 번호 API 변경된 Response 포맷 적용

--- app/src/main/java/com/lottomate/lottomate/data/remote/api/MyNumberApi.kt
+++ app/src/main/java/com/lottomate/lottomate/data/remote/api/MyNumberApi.kt
@@ -2,6 +2,7 @@ package com.lottomate.lottomate.data.remote.api
 
 import com.lottomate.lottomate.data.model.MyNumberEntity
 import com.lottomate.lottomate.data.remote.request.RegisterLottoNumberEntity
+import com.lottomate.lottomate.data.remote.response.ResponseMyNumber
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
@@ -10,15 +11,15 @@ import retrofit2.http.Path
 
 interface MyNumberApi {
     @GET("my-lotto-numbers")
-    suspend fun getAllMyNumbers(): List<MyNumberEntity>
+    suspend fun getAllMyNumbers(): ResponseMyNumber<List<MyNumberEntity>>
 
     @POST("my-lotto-numbers")
     suspend fun insertMyNumber(
         @Body body: RegisterLottoNumberEntity,
-    ): Int?
+    ): ResponseMyNumber<Int?>
 
     @DELETE("my-lotto-numbers/{numberId}")
     suspend fun deleteMyNumber(
         @Path("numberId") id: Int,
-    ): Int?
+    ): ResponseMyNumber<Int?>
 }
